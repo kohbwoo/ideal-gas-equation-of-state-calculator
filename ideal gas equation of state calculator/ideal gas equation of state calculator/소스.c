760a559fff163ee45b90463ed93a75bd6267e8de
ByungWoo Koh <kohbwoo@gmail.com>
2019-06-16 16:27:38 +0900

    Add files via upload

--- ideal gas equation of state calculator/ideal gas equation of state calculator/소스.c
+++ ideal gas equation of state calculator/ideal gas equation of state calculator/소스.c
@@ -1,9 +1,58 @@
 //2019-06-14 부터 제작...
 #include <stdio.h>
 #include <windows.h>
-double R = 0.0820547;
+double R = 0.082006;
 double P, V, n, T;
 char find;
+void P_calculator();
+void V_calculator();
+void n_calculator();
+void R_calculator();
+void T_calculator();
+
+int main(void)
+{
+	char A;
+
+start:
+
+	printf("이상기체 상태방정식 계산기\n\n\n");
+	printf("기체상수 R은 %lf 으로 계산됩니다.\n", R);
+	printf("R값을 수정하시려면R 을 입력해주세요\n");
+	printf("단위는 atm(기압), L(부피), mol(분자 수), K(온도)를 사용합니다.\n\n\n");
+	printf("계산이 필요한 수식의 번호를 입력하세요.\n\n\n");
+	printf("번호\t방정식\n\n");
+	printf("1\tP값 구하기\n\n");
+	printf("2\tV값 구하기\n\n");
+	printf("3\tn값 구하기\n\n");
+	printf("4\tT값 구하기\n\n");
+	printf("5\t기체상수 R 구하기\n\n");
+	scanf("%c", &A);
+
+	system("cls");
+
+
+
+	if (A == '1') 
+		P_calculator();
+	else if (A == '2') 
+		V_calculator();
+	else if (A == '3') 
+		n_calculator();
+	else if (A == '4') 
+		T_calculator();
+	else if (A == '5')
+		R_calculator();
+	else if (A == 'R'||'r') {
+		printf("희망하시는 R값을 입력하세요:  ");
+		scanf("%lf", &R);
+		goto start;
+	}
+
+	else {
+		goto start;
+	}
+}
 
 void P_calculator() { //미지수 P가 필요할때
 	printf("P(기압)을 계산합니다.\n");
@@ -20,7 +69,6 @@ void P_calculator() { //미지수 P가 필요할때
 	return 0;
 
 }
-
 void V_calculator() { //미지수 V가 필요할때
 	printf("V(기압)을 계산합니다.\n");
 	printf("P값을 입력하시오:  ");
@@ -36,7 +84,6 @@ void V_calculator() { //미지수 V가 필요할때
 	return 0;
 
 }
-
 void n_calculator() { //미지수 n이 필요할때
 	printf("n(몰수)값을 계산합니다.\n");
 	printf("P값을 입력하시오:  ");
@@ -52,7 +99,6 @@ void n_calculator() { //미지수 n이 필요할때
 	return 0;
 
 }
-
 void T_calculator() { //미지수 T가 필요할때
 	printf("T(기압)을 계산합니다.\n");
 	printf("P값을 입력하시오:  ");
@@ -67,7 +113,6 @@ void T_calculator() { //미지수 T가 필요할때
 	return 0;
 
 }
-
 void R_calculator() { //미지수 R이 필요할때
 	printf("r(기체상수)을 계산합니다.\n");
 	printf("P값을 입력하시오:  ");
@@ -80,56 +125,8 @@ void R_calculator() { //미지수 R이 필요할때
 	scanf("%lf", &T);
 	R = P * V / (n * T);
 	printf("\nR값은 %lf 입니다.", R);
-	
+
 
 	return 0;
 
 }
-
-
-int main(void)
-{
-	char A;
-
-start:
-
-	printf("이상기체 상태방정식 계산기\n\n\n");
-
-
-	printf("기체상수 R은 %lf 으로 계산됩니다.\n", R);
-	printf("R값을 수정하시려면R 을 입력해주세요\n\n\n");
-
-	printf("계산이 필요한 수식의 번호를 입력하세요.\n\n\n");
-	printf("1\tP값 구하기\n\n");
-	printf("2\tV값 구하기\n\n");
-	printf("3\tn값 구하기\n\n");
-	printf("4\tT값 구하기\n\n");
-	printf("5\t기체상수 R 구하기\n\n");
-	scanf("%c", &A);
-	system("cls");
-
-	scanf("%c", &find);
-
-	if (A == '1') 
-		P_calculator();
-	else if (A == '2') 
-		V_calculator();
-	else if (A == '3') 
-		n_calculator();
-	else if (A == '4') 
-		T_calculator();
-	else if (A == '5')
-		R_calculator();
-
-	else if (A == 'R'||'r') {
-		printf("희망하시는 R값을 입력하세요.");
-		scanf("%lf", &R);
-		goto start;
-	}
-
-	else {
-		printf("잘못 입력하셧습니다.위 목록중 입력해주세요.\n\n\n");
-		goto start;
-	}
-}
-
